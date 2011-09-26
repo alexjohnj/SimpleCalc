e5616e76e171cfae0d03e07abaffcbb98d3beac2
Alex Jackson <ajamicr@gmail.com>
2011-09-26 19:28:32 +0100

    Created an application delegate for SimpleCalc (at last!)

---
+++ SimpleCalcAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SimpleCalcAppDelegate.h
+//  SimpleCalc
+//
+//  Created by Alex Jackson on 26/09/2011.
+//  Copyright 2011 SimpleCode. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimpleCalcAppDelegate : NSObject <NSApplicationDelegate>{
+    IBOutlet NSWindow *window;
+}
+@property (assign) IBOutlet NSWindow *window;
+@end
---
+++ SimpleCalcAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  SimpleCalcAppDelegate.m
+//  SimpleCalc
+//
+//  Created by Alex Jackson on 26/09/2011.
+//  Copyright 2011 SimpleCode. All rights reserved.
+//
+
+#import "SimpleCalcAppDelegate.h"
+
+@implementation SimpleCalcAppDelegate
+@synthesize window;
+
+-(void)applicationDidFinishLaunching:(NSNotification *)notification{
+    
+}
+
+@end
